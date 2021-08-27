a377a4f630cb51ed42b7f5d70fdff632caa6c646
heis-en-berg <heisenbergknocked@gmail.com>
2021-08-26 23:17:26 -0400

    Added type post with crud ops

--- x/blog/genesis.go
+++ x/blog/genesis.go
@@ -10,6 +10,13 @@ import (
 // state.
 func InitGenesis(ctx sdk.Context, k keeper.Keeper, genState types.GenesisState) {
 	// this line is used by starport scaffolding # genesis/module/init
+	// Set all the post
+	for _, elem := range genState.PostList {
+		k.SetPost(ctx, *elem)
+	}
+
+	// Set post count
+	k.SetPostCount(ctx, genState.PostCount)
 
 	k.SetPort(ctx, genState.PortId)
 	// Only try to bind to port if it is not already bound, since we may already own
@@ -29,6 +36,15 @@ func ExportGenesis(ctx sdk.Context, k keeper.Keeper) *types.GenesisState {
 	genesis := types.DefaultGenesis()
 
 	// this line is used by starport scaffolding # genesis/module/export
+	// Get all post
+	postList := k.GetAllPost(ctx)
+	for _, elem := range postList {
+		elem := elem
+		genesis.PostList = append(genesis.PostList, &elem)
+	}
+
+	// Set the current count
+	genesis.PostCount = k.GetPostCount(ctx)
 
 	genesis.PortId = k.GetPort(ctx)
 
